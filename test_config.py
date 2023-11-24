eea4c98553f1ecc84cd36bc0c58f66360d74ad69
Ariel <99064413+arielwhittingham@users.noreply.github.com>
2023-11-24 15:08:26 -0500

    update

--- test_config.py
+++ test_config.py
@@ -3,6 +3,7 @@ import os
     Airflow:
         https://www.youtube.com/watch?v=z7xyNOF8tak
         https://airflow.apache.org/docs/apache-airflow/1.10.4/start.html
+        https://airflow.apache.org/docs/apache-airflow/stable/tutorial/pipeline.html
     $ airflow webserver -p 8080
     $AIRFLOW_HOME=/Users/arielwhittingham/workspace/airflow     
     DBT:
