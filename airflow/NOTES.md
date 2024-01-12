6bf16bdf698fe887b9b72b5ea1a15bc7c9712cc7
Ariel <99064413+arielwhittingham@users.noreply.github.com>
2024-01-11 20:45:40 -0500

    added test script to airflow dag

---
+++ airflow/NOTES.md
@@ -0,0 +1,30 @@
+## Deployment and testing notes
+
+### Manual/local Airflow run/setup
+helpful commands
+
+
+### Cheat sheet for commands:
+```
+airflow cheat-sheet
+```
+
+```
+https://airflow.apache.org/docs/apache-airflow/stable/start.html
+
+check:
+# from ~wokspace/airflow directory:
+
+export AIRFLOW_HOME=~/workspace/airflow
+echo $AIRFLOW_HOME
+
+run:
+airflow db init && airflow webserver -p 8080 && airflow scheduler
+
+
+
+
+```
+
+
+### Docker Airflow run/setup
--- airflow/scratch/spotify_conn_scratch.py
+++ airflow/scratch/spotify_conn_scratch.py
@@ -28,19 +28,27 @@ def get_recently_played(token, start_ts):
     result = requests.get(query_url, headers=headers)
     return result
 
-def main():
+def main(run_type = "local"):
     sp = spotify_auth.SpotifyAPI()
     result = get_recently_played(sp.get_auth_token(),get_recent())
     try:
         f = os.makedirs("../data")
     except:
         print("Directory already exists")
-
-    with open(os.path.join("../data","spotify_sample_data"+datetime.datetime.strftime(datetime.datetime.now(),"_%Y-%m-%d %H:%M:%S.json")),"w") as jsonf:
+    with open(os.path.join("../data",f"spotify_sample_data_from_{run_type}"+datetime.datetime.strftime(datetime.datetime.now(),"_%Y-%m-%d %H:%M:%S.json")),"w") as jsonf:
         json.dump(result.json(),jsonf)
         jsonf.close()
 
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        if len(sys.argv) == 1:
+            main()
+        elif len(sys.argv) == 2:
+            main(sys.argv[1])
+        else:
+            raise Exception("Usage: python3 spotify_conn_scratch.py [run_type]. use either 0 or 1 command line argument")
+    except Exception as e:
+        print(e)
+        exit(1)
